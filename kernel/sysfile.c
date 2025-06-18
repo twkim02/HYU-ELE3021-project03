c8b5751d3e2fde5ed69369e2d7f521aade047545
KIM TAEWOO <mathtw@naver.com>
2025-06-18 22:16:18 +0900

    2021052451-final

--- kernel/sysfile.c
+++ kernel/sysfile.c
@@ -311,6 +311,7 @@ sys_open(void)
   struct file *f;
   struct inode *ip;
   int n;
+  int depth, len;
 
   argint(1, &omode);
   if((n = argstr(0, path, MAXPATH)) < 0)
@@ -330,6 +331,40 @@ sys_open(void)
       return -1;
     }
     ilock(ip);
+
+    if ((ip->type == T_SYMLINK) && !(omode & O_NOFOLLOW)){
+      depth = 0;
+      while (ip->type == T_SYMLINK && depth < 10) {
+        len = 0;
+
+        if(readi(ip, 0, (uint64)&len, 0, sizeof(int)) != sizeof(int))
+          panic("readi error: read len");
+
+        if(len >= MAXPATH){
+          iunlockput(ip);
+          end_op();
+          return -1;
+        }
+
+        if(readi(ip, 0, (uint64)path, sizeof(int), len+1) != len+1)
+          panic("readi error: read path");
+
+        iunlockput(ip);
+
+        if((ip = namei(path)) == 0){
+          end_op();
+          return -1;
+        }
+        ilock(ip);
+        depth++;
+      }
+      if (depth >= 10) {
+        iunlockput(ip);
+        end_op();
+        return -1;
+      }
+    }
+    
     if(ip->type == T_DIR && omode != O_RDONLY){
       iunlockput(ip);
       end_op();
@@ -511,13 +546,30 @@ sys_symlink(void)
 {
   char target[MAXPATH];
   char path[MAXPATH];
+  struct inode *ip;
+  int len;
 
   if (argstr(0, target, MAXPATH) < 0 || argstr(1, path, MAXPATH) < 0)
     return -1;
 
-  begin_op(); // transaction start
+  begin_op();
 
-  // TODO
+  ip = create(path, T_SYMLINK, 0, 0);
+  if(ip == 0){
+    end_op();
+    return -1;
+  }
+
+  len = strlen(target);
+
+  if(writei(ip, 0, (uint64)&len, 0, sizeof(int)) != sizeof(int))
+    panic("writei error: write len");
+
+  if(writei(ip, 0, (uint64)target, sizeof(int), len+1) != len+1)
+    panic("writei error: write target");
+
+  iupdate(ip);
+  iunlockput(ip);
 
   end_op();
   return 0;
